b6305aba484d59d97524c45569c4d192b31955c7
Ben Bemis <bbemis@purdue.edu>
2016-07-01 23:35:44 -0400

    deployment update

---
+++ scripts/install.sh
@@ -0,0 +1,8 @@
+sh install_smarty.sh
+sh db_setup.sh
+
+#for .htaccess files
+a2enmod rewrite
+
+#set file permissions
+sh set_permissions.sh
--- scripts/install_smarty.sh
+++ scripts/install_smarty.sh
@@ -1,4 +1,5 @@
 #download and extract Smarty
+mkdir ~/Downloads
 cd ~/Downloads
 wget https://github.com/smarty-php/smarty/archive/v3.1.29.tar.gz
 tar -xzf v3.1.29.tar.gz
@@ -6,5 +7,5 @@ mkdir /usr/lib/php/7.0/Smarty
 cp -r smarty-3.1.29/libs/* /usr/lib/php/7.0/Smarty
 
 #setup directories in LivestockTracker
-#cd /var/www/LivestockTracker/src/private/views
-#mkdir cache configs smarty templates
+cd /var/www/LivestockTracker/src/private/views
+mkdir cache configs smarty templates
